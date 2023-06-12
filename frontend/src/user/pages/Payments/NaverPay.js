3ca4697488cd0fb332141d7006dc83ea27dfdd26
DESKTOP-P141EGC\kosmo <rlawhdgh1192@gmail.com>
2023-06-12 18:33:09 +0900

    6.12 진전사항없음 NavigationBar #수정

--- frontend/src/user/pages/Payments/NaverPay.js
+++ frontend/src/user/pages/Payments/NaverPay.js
@@ -4,15 +4,10 @@ import './NaverPay.css';
 const NaverPay = () => {
   const [selectedAmount, setSelectedAmount] = useState(0);
 
-  const toggleButton = (button, amount) => {
-    setSelectedAmount(amount);
-  };
+
 
   const handleNaverPayBtnClick = () => {
-    if (selectedAmount < 100) {
-      alert('결제금액은 100 이상이어야 합니다.');
-      return;
-    }
+
 
     const oPay = window.Naver.Pay.create({
       mode: 'development', // development or production
@@ -37,8 +32,8 @@ const NaverPay = () => {
       merchantUserKey: '4648484646464',
       merchantPayKey: '7864546465465465',
       productName: '매치기릿',
-      totalPayAmount: selectedAmount,
-      taxScopeAmount: selectedAmount,
+      totalPayAmount: '200',
+      taxScopeAmount: '200',
       taxExScopeAmount: '0',
       returnUrl: '사용자 결제 완료 후 결제 결과를 받을 URL',
     });
@@ -57,36 +52,7 @@ const NaverPay = () => {
 
   return (
     <div>
-        <button className={`button ${selectedAmount === 2000 ? 'clicked' : ''}`} onClick={() => toggleButton(2000, 2000)}>
-        2,000원
-        </button>
-        <button className={`button ${selectedAmount === 5000 ? 'clicked' : ''}`} onClick={() => toggleButton(5000, 5000)}>
-        5,000원
-        </button>
-        <button className={`button ${selectedAmount === 10000 ? 'clicked' : ''}`} onClick={() => toggleButton(10000, 10000)}>
-        10,000원
-        </button>
-        <br />
-        <button className={`button ${selectedAmount === 20000 ? 'clicked' : ''}`} onClick={() => toggleButton(20000, 20000)}>
-        20,000원
-        </button>
-        <button className={`button ${selectedAmount === 30000 ? 'clicked' : ''}`} onClick={() => toggleButton(30000, 30000)}>
-        30,000원
-        </button>
-        <button className={`button ${selectedAmount === 50000 ? 'clicked' : ''}`} onClick={() => toggleButton(50000, 50000)}>
-        50,000원
-        </button>
-        <br />
-        <button className={`button ${selectedAmount === 80000 ? 'clicked' : ''}`} onClick={() => toggleButton(80000, 80000)}>
-        80,000원
-        </button>
-        <button className={`button ${selectedAmount === 90000 ? 'clicked' : ''}`} onClick={() => toggleButton(90000, 90000)}>
-        90,000원
-        </button>
-        <button className={`button ${selectedAmount === 100000 ? 'clicked' : ''}`} onClick={() => toggleButton(100000, 100000)}>
-        100,000원
-        </button>
-        <br />
+
         <input type="button" id="naverPayBtn" value="네이버페이 결제 버튼" onClick={handleNaverPayBtnClick} />
         <div id="paymentInfo">
             <p>결제 ID:</p>
